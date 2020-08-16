479ed08ae42e24b31405e79b980de2e8735309c9
Qv2ray-dev <59914293+Qv2ray-dev@users.noreply.github.com>
2020-08-16 16:50:11 +0800

    rename: renamed CMake Project Name

--- ui/PluginMainWindowWidget.hpp
+++ ui/PluginMainWindowWidget.hpp
@@ -13,7 +13,19 @@ class SimplePluginMainWindowWidget
     explicit SimplePluginMainWindowWidget(QWidget *parent = nullptr);
     const QList<QMenu *> GetMenus() override
     {
-        return {};
+        const auto items = { "Item1", "Item2", "Item3", "Item4", "Item5" };
+        //
+        auto restaurants = new QMenu("Restaurants", this);
+        auto toilets = new QMenu("Toilets", this);
+        auto hotels = new QMenu("Hotels", this);
+        //
+        for (const auto &item : items)
+        {
+            restaurants->addAction(item);
+            toilets->addAction(item);
+            hotels->addAction(item);
+        }
+        return { restaurants, toilets, hotels };
     }
 
   protected:

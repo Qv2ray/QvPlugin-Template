bda871be4b1d84872e463c4e62fb08163902d8de
QxQ <59914293+U-v-U@users.noreply.github.com>
2020-09-07 22:04:00 +0800

    Update SimplePlugin.cpp

--- SimplePlugin.cpp
+++ SimplePlugin.cpp
@@ -6,10 +6,10 @@
 #include "core/SubscriptionAdapter.hpp"
 #include "ui/Interface.hpp"
 
-bool SimplePlugin::InitializePlugin(const QString &, const QJsonObject &)
+bool SimplePlugin::InitializePlugin(const QString &, const QJsonObject &_settings)
 {
     emit PluginLog("Initialize plugin.");
-    this->settings = settings;
+    this->settings = _settings;
     outboundHandler = std::make_shared<SimpleSerializer>();
     eventHandler = std::make_shared<SimpleEventHandler>();
     kernelInterface = std::make_shared<SimpleKernelInterface>();
